c658ca79ab24800ab5bbde6e56343cfa4a5e6dfe
alonb <alonb@kontera.com>
2014-08-12 11:04:48 +0300

    added support for version 1.7 coffeescript

--- test/stackTraceTest.js
+++ test/stackTraceTest.js
@@ -2,6 +2,9 @@ var coffee = require("../lib/coffee-script-mapped");
 var test =require("./coffeeTest");
 var expect = require("chai").expect;
 
+var coffeeVersion = coffee.VERSION.match(/(\d)\.(\d)/);
+var versionAbove17;
+versionAbove17 = coffeeVersion[1] > 1 || (coffeeVersion[1] == 1 && coffeeVersion[2] > 6);
 describe("stack trace test", function () {
     describe("sourcemaps on", function () {
         it("should show proper coffee stack", function () {
@@ -9,8 +12,12 @@ describe("stack trace test", function () {
                 test.innerErr()
             } catch (err) {
                 var lines = err.stack.split("\n")
-                expect(lines[1]).to.match(/coffeeTest\.coffee:3:14\)$/)
-                expect(lines[2]).to.match(/stackTraceTest\.js:9:22\)$/)
+		if (versionAbove17) {
+                	expect(lines[1]).to.match(/coffeeTest\.coffee:3:15\)$/)
+		} else {
+                	expect(lines[1]).to.match(/coffeeTest\.coffee:3:14\)$/)
+		}
+                expect(lines[2]).to.match(/stackTraceTest\.js:12:22\)$/)
             }
         })
         it("should show proper coffee stack with nested calls", function () {
@@ -18,8 +25,13 @@ describe("stack trace test", function () {
                 test.outerErr()
             } catch (err) {
                 var lines = err.stack.split("\n")
-                expect(lines[1]).to.match(/coffeeTest\.coffee:3:14\)$/)
-                expect(lines[2]).to.match(/coffeeTest\.coffee:5:4\)$/)
+		if (versionAbove17) {
+			expect(lines[1]).to.match(/coffeeTest\.coffee:3:15\)$/)
+                	expect(lines[2]).to.match(/coffeeTest\.coffee:5:13\)$/)
+		} else {
+	                expect(lines[1]).to.match(/coffeeTest\.coffee:3:14\)$/)
+			expect(lines[2]).to.match(/coffeeTest\.coffee:5:4\)$/)
+		}
             }
         })
         it("should show proper coffee stack with multiple files", function () {
@@ -27,8 +39,13 @@ describe("stack trace test", function () {
                 test.nestedErr()
             } catch (err) {
                 var lines = err.stack.split("\n")
-                expect(lines[1]).to.match(/anotherErr\.coffee:2:14\)$/)
-                expect(lines[2]).to.match(/coffeeTest\.coffee:7:4\)$/)
+		if (versionAbove17) {
+			expect(lines[1]).to.match(/anotherErr\.coffee:2:15\)$/)
+               		expect(lines[2]).to.match(/coffeeTest\.coffee:7:16\)$/)
+		} else {
+	                expect(lines[1]).to.match(/anotherErr\.coffee:2:14\)$/)
+	                expect(lines[2]).to.match(/coffeeTest\.coffee:7:4\)$/)
+		}
             }
         })
 		it("should show proper coffee stack when we have syntax error", function() {
